db2f3bf7b561f384c79ef4c6a936a63b48506947
yeonun <yekang.cau@gmail.com>
2021-04-27 20:41:56 +0900

    make posts list

---
+++ tutorial-SimpleBlog/blog/src/App.js
@@ -0,0 +1,58 @@
+/* eslint-disable */
+
+import logo from "./logo.svg";
+import "./App.css";
+import React, {useState} from 'react';
+
+function App() {
+  let posts = "시작하기전 프로젝트 세팅하기";
+  let [title, setTitle] = useState(['시작하기전 프로젝트 세팅하기','state에 데이터를 저장하기', '저녁 메뉴를 고르는 방법']);
+  let postTitleStyle = { color: " black ", fontSize: "25px" };
+
+  let [likey, setLikey] = useState(0);
+
+  function setTitleClick() {
+    var newTitle = [...title];
+    newTitle[2] = "점심 메뉴를 고르는 방법";
+    setTitle(newTitle);
+  }
+
+  function arrangeClick() {
+    var arrangeTitle = [...title];
+    arrangeTitle = arrangeTitle.sort();
+    setTitle(arrangeTitle);
+  }
+
+  function likeUpClick() {
+    setLikey(likey+1);
+  }
+
+  return (
+    <div className="App">
+      <div className="Black-nav">
+        <div> yeonun의 연습장</div>
+      </div>
+      <button onClick = { setTitleClick }> 제목을 바꾸는 버튼 </button>
+      <button onClick = { arrangeClick }> 글 정렬 하는 버튼 </button>
+      <div className="postList">
+        <h3 style = { postTitleStyle }> { title[0] }
+        <span onClick={ likeUpClick }> 👍 </span> {likey}
+        </h3>
+        <p> 4월 27일 작성 </p>
+        <hr/>
+      </div>
+      <div className="postList">
+        <h3 style = { postTitleStyle }> { title[1] } </h3>
+        <p> 4월 28일 작성 </p>
+        <hr/>
+      </div>
+      <div className="postList">
+        <h3 style = { postTitleStyle }> { title[2] } </h3>
+        <p> 4월 29일 작성 </p>
+        <hr/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
