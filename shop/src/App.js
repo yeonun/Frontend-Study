7e9bc343655b80ab619663061c2516f89f9af08e
yeonun <yekang.cau@gmail.com>
2021-04-28 00:56:07 +0900

    make cards to componets

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,21 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Button,
-  Jumbotron
-} from "react-bootstrap";
-import React, {useState} from "react";
+import { Navbar, Nav, NavDropdown, Button, Jumbotron } from "react-bootstrap";
+import React, { useState } from "react";
 import reactDom from "react-dom";
-import Data from './data';
-
+import Data from "./data";
 
 function App() {
-
-  let [ data, setData ] = useState(Data);
+  let [data, setData] = useState(Data);
 
   return (
     <div className="App">
@@ -54,23 +46,30 @@ function App() {
 
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img src="https://i.pinimg.com/originals/eb/53/88/eb538826c39467b9a20d5b6257175107.jpg" width="100%" />
-            <h4> {data[0].title} </h4>
-            <p> {data[0].content} & {data[0].price} </p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://i.pinimg.com/736x/f9/93/12/f99312310bf429bc11546a5b2b9ae887.jpg" width="100%" />
-            <h4> {data[1].title} </h4>
-            <p> {data[1].content} & {data[1].price} </p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://i.pinimg.com/736x/1e/32/d9/1e32d9150dd3670ef84ec8dc81b7a5bd.jpg" width="100%" />
-            <h4> {data[2].title} </h4>
-            <p> {data[2].content} & {data[2].price} </p>
+          {
+            data.map((num,i)=>{
+              return(
+              <Card data={data[i]} i={i}/>
+              );
+            })
+          }
           </div>
         </div>
       </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src = {'https://codingapple1.github.io/shop/shoes'+(props.i+1)+'.jpg'}
+        width="100%"
+      />
+      <h4> {props.data.title} </h4>
+      <p>
+        {props.data.content} & {props.data.price}
+      </p>
     </div>
   );
 }
